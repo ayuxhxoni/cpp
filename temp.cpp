99a1d25651ec3ee53ea46ff99f783315226f7596
Lucierzz <81070420+Lucierzz@users.noreply.github.com>
2022-08-30 11:44:46 +0530

    functions

--- temp.cpp
+++ temp.cpp
@@ -1,7 +1,13 @@
+#include<math.h>    
 #include<bits/stdc++.h> 
 using namespace std;
 int main(){
-    cout<<"hello world"<< endl;
-    cout<<" "
+    cout<<"what's your name?"<<endl;
+    string name;
+    getline(cin,name);
+    cout<<"what do you want to order?"<<endl;
+    string order;
+    getline(cin,order);
+    cout<<name<<" your order "<<order<< " will be ready in 5 min"<<endl;
     return 0;
 }
