42d838875ab88baac27813db0f25714f564a87e8
Lucierzz <81070420+Lucierzz@users.noreply.github.com>
2022-08-30 12:06:40 +0530

    if

---
+++ ifels.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int a,b,c;
+
+int main(){
+    cin>>a>>b>>c;
+    if(a>b){
+        cout<<"the value "<<a<< " is of variable a"<<endl;
+    }
+
+
+    return 0;
+}
